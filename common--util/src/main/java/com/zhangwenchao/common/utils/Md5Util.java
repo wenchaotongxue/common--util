a38ef14ced0076d9935ae4487d03aed01a8c62d3
wenchao <848105486@qq.com>
2019-12-16 19:18:17 +0800

    添加MD5Util

---
+++ common--util/src/main/java/com/zhangwenchao/common/utils/Md5Util.java
@@ -0,0 +1,57 @@
+package com.zhangwenchao.common.utils;
+
+import java.security.MessageDigest;
+
+/**
+ * 
+ * @author 作者 Your-Name: zhangwenchao
+ * 
+ * @version 创建时间：2019年12月16日 下午7:14:35
+ * 
+ *          类说明 采用Md5加密解密
+ * 
+ */
+public class Md5Util {
+
+	public static String string2MD5(String inStr) {
+		MessageDigest md5 = null;
+		try {
+			md5 = MessageDigest.getInstance("MD5");
+		} catch (Exception e) {
+			System.out.println(e.toString());
+			e.printStackTrace();
+
+			return "";
+
+		}
+		char[] charArray = inStr.toCharArray();
+		byte[] byteArray = new byte[charArray.length];
+
+		for (int i = 0; i < charArray.length; i++)
+			byteArray[i] = (byte) charArray[i];
+		byte[] md5Bytes = md5.digest(byteArray);
+		StringBuffer hexValue = new StringBuffer();
+		for (int i = 0; i < md5Bytes.length; i++) {
+			int val = ((int) md5Bytes[i]) & 0xff;
+			if (val < 16)
+				hexValue.append("0");
+			hexValue.append(Integer.toHexString(val));
+		}
+		return hexValue.toString();
+
+	}
+
+	/**
+	 * @Title: convertMD5 @Description: 加密解密算法 执行一次加密，两次解密 @param: @param
+	 * inStr @param: @return @return: String @throws
+	 */
+	public static String convertMD5(String inStr) {
+		char[] a = inStr.toCharArray();
+		for (int i = 0; i < a.length; i++) {
+			a[i] = (char) (a[i] ^ '9');
+		}
+		String s = new String(a);
+		return s;
+	}
+
+}
